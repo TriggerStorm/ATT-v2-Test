4d7dfbd789a7c98c74f56276805e5f3591dae368
TriggerStorm <michael.h.p@hotmail.com>
2020-02-07 08:13:14 +0100

    Initial commit

---
+++ src/att/v2/test/FXML_GeneratedCodeController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package att.v2.test;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Trigger
+ */
+public class FXML_GeneratedCodeController implements Initializable {
+
+    @FXML
+    private TextField TF_code;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
